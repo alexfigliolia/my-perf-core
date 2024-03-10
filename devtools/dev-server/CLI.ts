f2a2039d874578d0afce443bad30a05318b037a7
Alex Figliolia <alexfigliolia@gmail.com>
2024-03-10 15:55:35 -0700

    Migrate to shared dev server

--- devtools/dev-server/CLI.ts
+++ devtools/dev-server/CLI.ts
@@ -1,5 +1,21 @@
-import { Development } from "./Development";
+import { DevServer } from "@figliolia/typescript-dev-server";
 
 (async () => {
-  await Development.run();
+  const Server = new DevServer({
+    entryPoint: "src/Start.ts",
+    serviceCommands: {
+      Redis: "brew services start redis",
+      Postgres: "brew services start postgresql@16",
+    },
+    killCommands: {
+      Redis: "brew services stop redis",
+      Postgres: "brew services stop postgresql@16",
+    },
+    nodeOptions: {
+      NODE_ENV: "development",
+      NODE_OPTIONS: "--enable-source-maps",
+      NODE_EXTRA_CA_CERTS: "./cert/server.cert",
+    },
+  });
+  await Server.run();
 })().catch(console.log);
